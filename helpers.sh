b2e02fe08c3675cb5bc523a5e3e7abdedb99e9ef
Michał Janke <michal.janke@mendix.com>
2021-06-03 16:42:08 +0200

    Add shell helper function set for dev purposes
    Possibly only temporary - may be useful to get integrated into "deform"
    command as one of subcommands (TBD).
    
    Curently only a function that streamlines creation of raw->provider
    translation files.

---
+++ helpers.sh
@@ -0,0 +1,85 @@
+_yaml_header()
+{
+  local K=$1
+  cat << YAML
+source:
+  apiVersion: "raw.import.tf.xxx/v1alpha1" #TODO: Come up with reasonable api name
+  kind: $K
+target:
+  apiVersion: #XXX COPY-PASTE FROM https://doc.crds.dev/github.com/crossplane/provider-aws/ s3.aws.crossplane.io/ XXX v1beta1
+  kind: #XXX (GUESS) search for: ${K#Aws}
+imports:
+YAML
+}
+
+_yaml_import_expand()
+{
+  cat << YAML
+- from: "$1"
+  to: "spec.forProvider." #XXX
+YAML
+}
+
+_yaml_helpful_footer()
+{
+  cat << 'YAML'
+##### COPY-PASTE helpers ####
+  transforms:
+  - type: convert
+    convert:
+      toType: string
+  - map:
+      "false": "Disabled"
+      "true": "Enabled"
+    type: map
+YAML
+}
+
+prep_files () 
+{
+  local provider=aws
+  local generated=()
+
+  [ $# -eq 2 ] || {
+    echo "Usage: $0   <json_file>   <root-module|child-modules>"
+    return 1
+  }
+  
+  ## WHY like this? BECAUSE BASH! Cannot alter variables from a pipe.
+  ### See: https://unix.stackexchange.com/questions/224576/how-do-i-append-an-item-to-an-array-in-a-pipeline
+  while read lin
+  do
+    eval $lin
+    local yaml="${provider}/${kind}.yaml"
+    if [ -r ${yaml} ]; then 
+      echo " > ${yaml} already exists. SKIPPING.";
+      #ls ${yaml} | grep --color=always "${kind}.yaml"
+    else
+      echo -e "\n\e[44;1m>> Found ${#paths[@]} json paths in spec for $kind \e[0m";
+      echo " > generating ${yaml}"
+      _yaml_header $kind >> ${yaml}
+      for p in ${paths[@]}; do
+        _yaml_import_expand $p >> ${yaml}
+      done
+      _yaml_helpful_footer >> ${yaml}
+      echo " > ${yaml} generated"
+      generated+=("$yaml")
+      echo
+    fi
+  done < <(./deform $1 $2 xr \
+    | jq -r '
+      @sh "
+        kind=\(.kind)
+        paths=(\({spec}|[paths(scalars) | map(.|tostring) | join(".")]))
+        "
+      '
+  )
+
+  echo
+  echo "-+-+-+-+-+-+-+-+-+-+-+-+-"
+  echo " > Generated ${#generated[@]} new files."
+  echo "============ ${generated[@]}"
+  echo " > Press <Enter> to edit them one by one or <Ctrl-C> to skip."
+  read
+  for f in "${generated[@]}"; do vim $f; done
+}
