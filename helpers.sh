739701d0510e9116edf3aa2d62334b558635d295
Michał Janke <michal.janke@mendix.com>
2021-06-23 17:33:41 +0200

    helpers: Add "cover" and "cover_stats" functions
    Quick implementations of functions informing about current level of support
    for resources found in given "terraform show -json" files

--- helpers.sh
+++ helpers.sh
@@ -115,3 +115,50 @@ prep_files ()
     done
   fi
 }
+
+# cover ()
+# {
+#   ./deform <( cat $@ ) aws xr | jq '.kind' -r | sort | uniq -c | sort -g \
+#   | column -J --table-columns "count,kind" \
+#   | jq -c '.table[]|.status=(.|@sh "kAAAAAind=\(.kind); ls aws/${kind}.yaml")'
+#   #do
+#   #    echo -e "\e[35m >> \e[0m ${kind}($count)\ttesting...";
+#   #done \
+# }
+
+cover ()
+{
+  local provider=$1
+  if [ ! ${provider} ] || [ ! -d ${provider} ]
+  then
+    echo "Usage: $FUNCNAME <provider> FILES..."
+    return
+  fi
+  shift
+  ###XXX Consider: https://stackoverflow.com/questions/26717277/accessing-a-json-object-in-bash-associative-array-list-another-model/51690860#51690860
+  ./deform <( cat $@ ) ${provider} xr | jq '.kind' -r | sort | uniq -c | sort -g \
+    | while read count kind; do
+      echo -e "${kind}($count)\t$(ls ${provider}/${kind}.yaml 2>/dev/null || if [ -f ${provider}/_edit_${kind}.yaml ]; then echo -n "(WIP)"; else echo -n "(MISSING)"; fi )"
+    done \
+    | column -t \
+    | while IFS=$'\r\n' read -r LINE; do
+      echo -e "\e[35m >> \e[0m $LINE"
+    done
+}
+
+cover_stats()
+{
+  local provider=$1
+  if [ ! ${provider} ] || [ ! -d ${provider} ]
+  then
+    echo "Usage: $FUNCNAME <provider> FILES..."
+    return
+  fi
+  shift
+  for f in "$@"
+  do
+    #TODO: Optimize - remove second invocation of "cover"
+    cover ${provider} $f \
+      | echo -e "\e[35m >> ${f} \e[0m: $(grep -cF '.yaml')/$( cover ${provider} ${f} | wc -l  )"
+  done
+}
