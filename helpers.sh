bcbe5bf06a570bf0f853721bb25b5fce79625f5e
Michał Janke <michal.janke@mendix.com>
2021-07-06 16:26:29 +0200

    WIP: helper functions for api matching and coverage stats

--- helpers.sh
+++ helpers.sh
@@ -148,6 +148,26 @@ cover ()
 
 cover_stats()
 {
+  colorize()
+  {
+    local color=$1
+    local needle=$2
+    local col
+    local ret=$'\e[0m'
+    case $color in
+      RED)
+        col=$'\e[31;1m'
+        ;;
+      ORANGE)
+        col=$'\e[33;1m';
+        ;;
+      BLUE)
+        col=$'\e[34;1m';
+        ;;
+    esac
+    sed  -E "s#(${needle})#${col}\0${ret}#g"
+  }
+
   local provider=$1
   if [ ! ${provider} ] || [ ! -d ${provider} ]
   then
@@ -159,6 +179,162 @@ cover_stats()
   do
     #TODO: Optimize - remove second invocation of "cover"
     cover ${provider} $f \
-      | echo -e "\e[35m >> ${f} \e[0m: $(grep -cF '.yaml')/$( cover ${provider} ${f} | wc -l  )"
-  done
+    | echo ">> ${f} $(grep -cE '(.yaml$|WIP)')/$( cover ${provider} ${f} | wc -l  )" \
+    | colorize BLUE '>>' \
+    | sed -E 's#[^0-9]([0-9]+)/\1#[34;1m\0\t(ALL!)[0m#g'
+  done \
+  | column -t
+}
+
+#XXX pseudocode outline for utilizing docs (terraform-provider-scrape)
+#_decide_on_target ()
+#{
+#    if docs[input.type] has input.param in "args"; then
+#        output:;
+#        {
+#            - from: "spec.$input.param";
+#            to: "spec.forProvider.(toCamel input.param)"
+#        };
+#    else
+#        output:;
+#        {
+#            - from: "spec.$input.param";
+#            at: "status.atProvider.(toCamel input.param)"
+#        };
+#    fi
+#}
+#
+#
+#_find_param_candidate () #arg1: canditate_target_type
+#{
+#  get_provider_crd  canditate_target_type \
+#    | list_param_paths \
+#    | smart_grep   each param word split by "_"
+#
+##[IDEA]
+##  $ < provider-crds/aws_v0.19.0.json jq '.[]|.spec|[(.group/"."|.[0]),.names.kind]'
+## - though this includes things like
+## [
+##   "servicediscovery",
+##   "PublicDNSNamespace"
+## ]
+##
+## - it can be hard to split the camel-cased words... One would have to run
+## through the string, detect capital letters and in each case check
+## if the next letter isn't capital as well.
+#}
+
+extract_params ()
+{
+  local kind=$1
+  [ -f ${kind}.* ] || return
+  echo -n "{
+  \"kind\": \"${kind}\",
+  \"args\": ["
+  cat ${kind}.* \
+    | sed -n -e '/^##* Arguments* Ref/,/^#/p' \
+    | sed -e 's/"/\\"/g' \
+    | sed -n -E -e 's#^ *([*-] *)`([a-z][^`]*)`.*$#"\2",#p' \
+    | sort | uniq \
+    | tr '\n' ' ' \
+    | head -c-2
+  echo -n "],
+  \"attrs\": ["
+  cat ${kind}.* \
+    | sed -n -e '/^##* Attributes* Ref/,/^#/p' \
+    | sed -e 's/"/\\"/g' \
+    | sed -n -E -e 's#^ *([*-] *)`([a-z][^`]*)`.*$#"\2",#p' \
+    | sort | uniq \
+    | tr '\n' ' ' \
+    | head -c-2
+  echo "]"
+  echo "}"
+}
+
+#guess_config_for ()
+#{
+#  local kind=${1}
+#  local crds_file=${2}
+#  local kind_words=$(echo $kind | tr '_' ' ')
+#  local candidates=
+
+select_mappings ()
+{
+  split_words()
+  {
+    sed -e 's/[A-Z][a-z0-9]/.\0/g' -E -e 's/[A-Z][A-Z]+/.\0/g' | tr '[A-Z]' '[a-z]'
+  }
+  [ -f $1 ] || return 1
+  [ -f $2 ] || return 2
+  local crossplane_crds=$1
+  local terraform_specs=$2
+  [ $3 ] || return 3
+  local output=$3
+  shift 3
+  < ${crossplane_crds} jq '.[]|.spec|((.group/"."|.[0]) + " " + .names.kind)' -r \
+    | while read group kind
+      do
+        local grep_ext_regex
+        grep_ext_regex=$(< ${crossplane_crds} \
+          jq -c -r '
+            .[]
+            | select(.spec.names.kind=="'${kind}'")
+            | .spec.versions[0].schema.openAPIV3Schema
+                    | .properties.spec.properties
+                    | .forProvider.properties
+                    | keys
+            ' \
+          | sed -e 's/[A-Z][a-z0-9]/.\0/g' -E -e 's/[A-Z][A-Z]+/.\0/g' \
+          | tr '[A-Z]' '[a-z]' | tr '[],' '()|' \
+          | sed -e 's/"/\\b/g'
+        )
+        
+
+        echo ">>>> grep -E \"${grep_ext_regex}\""
+
+        #break #######################################
+        
+        ## TODO make it in a single call
+        echo -en "${group}.${kind}   " >> ${output}
+
+        #FIXME: all the subsequent jq parsings of $crossplane_crds are WRONG (they don't select group - e.g. Cluster...) and can be removed altogether
+        < ${terraform_specs} jq -c '.kind' -r \
+        | fzf -1 -i -q "${kind} ${group}" \
+            --no-info \
+            --reverse \
+            --preview-window=left,30% \
+            --preview="echo selected terraform module parameters:; < ${terraform_specs} jq -C 'select(.kind==\"'{}'\")' | grep --color=always -C99 -E \"${grep_ext_regex}\"" \
+            --header="$(
+                < ${crossplane_crds} \
+                  jq '
+                    .[]
+                    | select(.spec.names.kind=="'${kind}'")
+                    | .spec.versions[0].schema.openAPIV3Schema
+                    | .description';
+                echo -e "\e[44m=> arguments in spec.forProvider:\e[0m";
+                < ${crossplane_crds} \
+                  jq -C '
+                    .[]
+                    | select(.spec.names.kind=="'${kind}'")
+                    | .spec.versions[0].schema.openAPIV3Schema
+                    | .properties.spec.properties
+                    | .forProvider.properties 
+                    | keys';
+                echo -e "\e[44m=> parameters in status.atProvider:\e[0m";
+                < ${crossplane_crds} \
+                  jq -C '
+                    .[]
+                    | select(.spec.names.kind=="'${kind}'")
+                    | .spec.versions[0].schema.openAPIV3Schema.properties
+                    | .status.properties
+                    | .atProvider.properties 
+                    | keys';
+                echo -e \
+                  "\n\e[44m== select best matching terraform module from below list ==\e[0m\n "
+                )" \
+            $@ \
+        >> ${output}
+        sleep 1
+        #break #XXX
+      done
 }
